cbd8ac1ed92afa1a78fea9cfe8797bb09bbc08c2
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-05-10 10:05:10 +0300

    Added start_exchange_rate_scheduler to main func..

--- runner.py
+++ runner.py
@@ -1,18 +1,27 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
 
-from app.handlers import router
+from app.parser import start_exchange_rate_scheduler
 from config import TELEGRAM_BOT_TOKEN
-
-bot = Bot(token=TELEGRAM_BOT_TOKEN)
-dp = Dispatcher()
+from app.database.models import async_main
+from app.handlers import router
 
 
 async def main():
+    start_exchange_rate_scheduler()
+
+    await async_main()
+    bot = Bot(token=TELEGRAM_BOT_TOKEN)
+    dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
+if __name__ == "__main__":
+    # logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
